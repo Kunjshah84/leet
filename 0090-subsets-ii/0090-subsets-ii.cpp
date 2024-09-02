5c306fdad6491dbf811dbb4ffc65e99a097245fe
Kunjshah84 <155896950+Kunjshah84@users.noreply.github.com>
2024-09-03 01:46:56 +0530

    Time: 3 ms (56.57%), Space: 16.9 MB (5.13%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,22 +1,35 @@
 class Solution {
 
-    void get(vector<int> nums,vector<int> temp,int ind,vector<vector<int>> &ans){
-        ans.push_back(temp);
-        for(int i=ind;i<nums.size();i++){
-            if(i>ind && nums[i]==nums[i-1])
-                continue ;
-            temp.push_back(nums[i]);
-            get(nums,temp,i+1,ans);
-            temp.pop_back();
+    void get(vector<int> nums,int ind,vector<vector<int>> &ans,bool flag,vector<int> temp){
+        if(ind==nums.size()){
+            ans.push_back(temp);
+            return ;
         }
-    }   
+        temp.push_back(nums[ind]);
+        if(ind==nums.size()-1){
+            if(flag) get(nums,ind+1,ans,1,temp);
+            temp.pop_back();
+            get(nums,ind+1,ans,1,temp);
+            return ;
+        }   
+        if(nums[ind]==nums[ind+1]){
+            if(flag) get(nums,ind+1,ans,1,temp);
+            temp.pop_back();
+            get(nums,ind+1,ans,0,temp);
+        }
+        else{
+            if(flag) get(nums,ind+1,ans,1,temp);
+            temp.pop_back();
+            get(nums,ind+1,ans,1,temp);
+        }
+    }
 
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> temp;
+        //In the different method without using the for loop!
         sort(nums.begin(),nums.end());
-        get(nums,temp,0,ans);
+        vector<vector<int>> ans;
+        get(nums,0,ans,1,{});
         return ans;
     }
 };
