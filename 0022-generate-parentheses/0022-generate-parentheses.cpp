d6fef17dbb37f0223e27c760e2237ee6dba84ee8
Kunjshah84 <155896950+Kunjshah84@users.noreply.github.com>
2024-09-05 10:36:13 +0530

    Time: 3 ms (60.74%), Space: 15.3 MB (45.73%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,27 +1,19 @@
 class Solution {
 
-    void get(int limit,vector<string> &ans,string temp,int op,int clo,int ind,int tc){
-        if(ind==limit*2){
-            if(tc==0)
-                ans.push_back(temp);
-            return ;
+    void get(int n,string temp,int op,int clo,vector<string> &ans){
+        if(temp.size()==n*2){
+            ans.push_back(temp);
+            return;
         }
-        if(op){
-            temp.push_back('(');
-            get(limit,ans,temp,op-1,clo,ind+1,tc+1);
-            temp.pop_back();
-        }
-        if(clo>0 && tc>0){
-            temp.push_back(')');
-            get(limit,ans,temp,op,clo-1,ind+1,tc-1);
-            temp.pop_back();
-        }
+        if(op<n) get(n,temp+'(',op+1,clo,ans);
+        if(clo<op) get(n,temp+')',op,clo+1,ans);
     }
 
 public:
     vector<string> generateParenthesis(int n) {
+        //This is the method of PW
         vector<string> ans;
-        get(n,ans,"(",n-1,n,1,1);
+        get(n,"",0,0,ans);
         return ans;
     }
 };
