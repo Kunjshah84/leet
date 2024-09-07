9dfeb8c66a3e3f576e1ff07bf81801717af20756
Kunj Shah <kunjshah294@gmail.com>
2024-09-08 02:28:49 +0530

    time pass

---
+++ stack_PW/astack_implementation_using_LL.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+//All the operation should be follow the time complexity of stack;
+
+class node{
+    public:
+    int data;
+    node* next;
+    node* head=NULL;
+    node(int value){
+        
+    }
+
+}
+
+int main(){
+
+    return ;
+}
