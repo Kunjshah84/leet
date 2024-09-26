ce9ff890a219b2bffd03410bc4d30e47cd9986f6
Kunjshah84 <155896950+Kunjshah84@users.noreply.github.com>
2024-09-26 20:02:51 +0530

    infix evaluation with breackets, what is ans prefix exp and what is the need of prefix is done

---
+++ PW_INFIX,POSTFIX,PREFIX/Infix_evaluation_with_bracket.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<stack>
+#include<string>
+using namespace std;
+
+int presi(char temp){
+    if(temp=='+' || temp=='-')
+        return 1;
+    return 2;
+}
+
+int cal(int v1,int v2,char temp){
+    if(temp=='+')
+        return v1+v2;
+    if(temp=='-')
+        return v1-v2;
+    if(temp=='*')
+        return v1*v2;
+    return v1/v2;
+}
+
+int main(){
+    stack<int> num;
+    stack<char> op;
+    string s="1+(2+6)*4/8-3";
+    cout<<"HELLo"<<endl;
+    for(int i=0;i<s.length();i++){
+        if(isdigit(s[i]))   
+            num.push(s[i]-48);
+        else if( !(s[i]==')') && (s[i]=='(' || op.empty() || presi(s[i])>presi(op.top())))
+            op.push(s[i]);
+        else{
+            if(s[i]==')'){
+                while(op.top()!='('){
+                    int v2=num.top();
+                    num.pop();
+                    int v1=num.top();
+                    num.pop();
+                    num.push(cal(v1,v2,op.top()));
+                    op.pop();
+                }
+                op.pop();
+            }
+            else{
+                while( !(op.empty()) && !(op.top()=='(') && presi(s[i])<=presi(op.top())){
+                    int v2=num.top();
+                    num.pop();
+                    int v1=num.top();
+                    num.pop();
+                    num.push(cal(v1,v2,op.top()));
+                    op.pop();
+                }
+                op.push(s[i]);
+            }
+        }
+    }
+    while(!op.empty()){
+        int v2=num.top();
+        num.pop();
+        int v1=num.top();
+        num.pop();
+        num.push(cal(v1,v2,op.top()));
+        op.pop();
+    }
+    cout<<"The ans of the infix is:-"<<num.top()<<endl;
+    return 0;
+}
---
+++ PW_INFIX,POSTFIX,PREFIX/prefix_expression_Intro.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<stack>
+#include<string>
+using namespace std;
+
+//Here is the difference between prefix and infix operation:
+//infix:v1 operator v2
+//prefix:operator v1 v2
+//posrfix:v1 v2 operator 
+
+//This is the simple example that shows how we can convert the infix operation to prefix operationl;
+//2+6---. + 2 6
+
+
+//Now we have to convert this string into prefix:
+//2+6*4/8-3 --->Infix:
+//convert a small sub string according to the oprder of presidene into prefix string:
+//now step 2 we got the step one is---> 2+*6 4/8-3
+//And now we treat the *6 4 as a seperate entity:
+
+//Now step 2 :2 + /* 6 4 8-3
+//Now step 3 :+ 2 /* 6 4 8 - 3 
+//Now step 4 : - + 2 /* 6 4 8 3--->At the end we have converted the infix to the post fix;
+//Now computer able to solve the prefix exp;
+
+
+//                             WHY computer converts infix to post of pre?
+
+//pre fix eleminates the need of the peranthisis and it also does not need the presidence table to evaluate the exp
+//Another reason is time and code complexity:
+//Pre solve the exp into only O(n) time complexity:
+
+
+int main(){
+
+    return 0;
+}
