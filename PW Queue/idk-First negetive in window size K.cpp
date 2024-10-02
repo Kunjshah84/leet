5c6a1cd5481caa541013a0750b8e9b3e5b5020c9
Kunjshah84 <155896950+Kunjshah84@users.noreply.github.com>
2024-10-02 21:33:13 +0530

    done one question of GFG

---
+++ PW Queue/idk-First negetive in window size K.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //Link of the question and it's solution:
+    // https://www.geeksforgeeks.org/problems/first-negative-integer-in-every-window-of-size-k3345/1
+    return 0;
+} 
