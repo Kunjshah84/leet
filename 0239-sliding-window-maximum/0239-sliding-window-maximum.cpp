60ebc7cb6b7aad3500a59dbffaee9a6760c28a10
Kunjshah84 <155896950+Kunjshah84@users.noreply.github.com>
2024-10-05 17:55:03 +0530

    Time: 149 ms (95.48%), Space: 137.4 MB (67.6%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,37 +1,15 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        vector<int> ans(nums.size());
-        stack<int> s;
-        s.push(nums.size()-1);
-        ans[nums.size()-1]=nums.size()-1;
-        for(int i=nums.size()-2;i>=0;i--){
-            while(!s.empty() && nums[i]>=nums[s.top()])
-                s.pop();
-            if(s.empty())   ans[i]=nums.size();
-            else    ans[i]=s.top();
-            s.push(i);
+        //Now in this we are going to use A monotonic Dequeue:
+        deque<int> d;
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+            while(!d.empty() && d.front()<i-k+1)    d.pop_front();
+            while(!d.empty() && nums[d.back()]<nums[i])   d.pop_back();
+            d.push_back(i);
+            if(i>=k-1)    ans.push_back(nums[d.front()]);
         }
-        cout<<"The vector is"<<endl;
-        for(auto it:ans)
-            cout<<it;
-        cout<<endl;
-        vector<int> fans(nums.size()-k+1,0);
-        int fi=0,li=nums.size()+1-k;
-        while(fi<li){
-            int i=fi;
-            if(ans[i]==(i+k-1))
-                fans[fi]=nums[i+k-1];
-            else if(ans[i]>(i+k-1))
-                fans[fi]=nums[fi];
-            else{
-                while(!(ans[i]>=(fi+k-1)))
-                    i=ans[i];
-                if(ans[i]==(fi+k-1))    fans[fi]=nums[fi+k-1];    
-                else    fans[fi]=nums[i];
-            }
-            fi++;
-        }
-        return fans;
+        return ans;
     }
 };
