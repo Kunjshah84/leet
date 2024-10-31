a3232b37108be37828a47ab1cb45e545031778ec
Kunjshah84 <155896950+Kunjshah84@users.noreply.github.com>
2024-10-31 20:07:46 +0530

    Time: 0 ms (100%), Space: 33.6 MB (5.09%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.cpp
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.cpp
@@ -12,9 +12,10 @@
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
-        if(!root)   return NULL;
-        if(root->val==val)  return root;
-        if(root->val>val)  return searchBST(root->left,val);
-        return searchBST(root->right,val);
+        while(root && root->val!=val){
+            if(root->val>val)   root=root->left;
+            else    root=root->right;
+        }
+        return root;
     }
 };
