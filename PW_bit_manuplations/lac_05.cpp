683431ed86d06bf960af6b34bb089b06fd50f4fb
Kunjshah84 <155896950+Kunjshah84@users.noreply.github.com>
2024-11-09 15:02:17 +0530

    flip the bit of the given decimal number and return number in the form of decimal number

---
+++ PW_bit_manuplations/lac_05.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+    int method_1_Pw(int n){
+        int ans=n;
+        n|=(n>>1);
+        n|=(n>>2);
+        n|=(n>>4);
+        n|=(n>>8);
+        n|=(n>>16);
+        return ans^n;
+    }
+
+    int method_2_Pw(int n){
+        //Use the brine method to find just smallest power of 2 and then substrct it with 1:--->
+        return 0;
+    }
+
+int main() {
+    //In this question given the number in decimal and flip all the bits into the number;
+    int n;
+    cout<<"Ente the value of any number:-"<<endl;
+    cin>>n;
+    //First approach-:
+    //make all bits with one in 32 bit binary number:-
+    cout<<"The result is:-"<<method_1_Pw(n)<<endl;
+    //ohk so the main goal to make all the bits one or we can say that generate the samllest power of 2
+    //among all the biggest power of 2 ans then subtract 1 from that number:--->boommmmm
+    cout<<"The result is:-"<<method_2_Pw(n)<<endl;
+    //We can use the brine algo to solve this question also because we want to find the just smallest pwo of 2:--->
+
+    return 0;
+}
