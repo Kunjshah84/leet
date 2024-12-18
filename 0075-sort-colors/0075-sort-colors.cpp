babad28bcd14129a4d334df54736f60cf53db7bd
Kunjshah84 <155896950+Kunjshah84@users.noreply.github.com>
2024-12-18 12:36:35 +0530

    Time: 0 ms (100%), Space: 11.6 MB (10.49%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,29 +1,17 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        //here i am using the three pointer approach:
-        //because here i have to arrange the 3 elements:
-        int v0=0,v1=0,var=0;
-        while(var<nums.size()){
-            if(nums[v0]==nums[v1]==0 && v0==v1){
-                v0++;
-                v1++;
-            }
-            else if(nums[v1]==1)    v1++;
-            else if(nums[var]==1){
-                swap(nums[v1],nums[var]);
-                v1++;
-            }
-            else if(nums[var]==0){
-                if(v0!=v1){
-                    swap(nums[v0],nums[var]);
-                    swap(nums[var],nums[v1]);
-                }
-                else    swap(nums[v0],nums[var]);
-                v0++;
-                v1++;
-            }
-            var++;
+        //Use the dutch national flag algo:
+        //take 3 var which is low mid and high
+        //and hypothetically assume that the from the 0 to low-1--->0's
+        //and from low to mid-1 there is onlyy--->1's
+        //and from the high+1 to size-1--->2's only
+        //and the unsorted portation is in between from the mid to high: 
+        int low=0,mid=0,high=nums.size()-1;
+        while(mid<=high){
+            if(nums[mid]==0)    swap(nums[low++],nums[mid++]);
+            else if(nums[mid]==1)   mid++;
+            else    swap(nums[high--],nums[mid]);
         }
     }
 };
