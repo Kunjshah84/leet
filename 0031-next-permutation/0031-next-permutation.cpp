968e4352a342f3b5ccf443413a7f8f36be026fb3
Kunjshah84 <155896950+Kunjshah84@users.noreply.github.com>
2024-12-19 23:41:29 +0530

    Time: 0 ms (100%), Space: 15.7 MB (11.84%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-        //Now we know that the previous one is striclly having decring sorted part
-        //so just change the position of the smallest biggest element with the first sorted one and after swap to make the smallest number rev the part which is previoslly unsorted: 
+    void nextPermutation(vector<int>& nums) { 
         if(nums.size()==1)  return ;
         int i=nums.size()-2,j=nums.size()-1;
         while(i>=0 && nums[i]>=nums[i+1])    i--;
-        if(i<0){
-            reverse(nums.begin(),nums.end());
-            return ;
+        if(i>=0){
+            while(nums[j]<=nums[i])  j--;
+            swap(nums[i],nums[j]);  
         }
-        while(nums[j]<=nums[i])  j--;
-        swap(nums[i],nums[j]);
         reverse(nums.begin()+i+1,nums.end());
         return ;
     }
