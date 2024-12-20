b63c12372878081962357ecf3e9b0ee684683d19
Kunjshah84 <155896950+Kunjshah84@users.noreply.github.com>
2024-12-20 10:26:18 +0530

    Time: 81 ms (52.32%), Space: 74.8 MB (40.12%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
+        //first we store all the unique elements into the unordered set
+        //then find the strating point by finding that if we had a smaller element there or not?
+        //the if we have the smalle one do nothing:
         if(!nums.size())    return 0;
-        sort(nums.begin(),nums.end());
-        int len=1,cnt=0;
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]-nums[i-1]==1)    cnt++;
-            else if(nums[i]-nums[i-1]>1){
-                len=max(cnt+1,len);
-                cnt=0;
+        unordered_set<int> s;
+        int ans=1;
+        for(auto it:nums)   s.insert(it);
+        for(auto it:s){
+            if(s.find(it-1)==s.end()){
+                int cnt=1,var=it;
+                while(s.find(var+1)!=s.end()){
+                    cnt++;
+                    var++;
+                }
+                ans=max(ans,cnt);
             }
-        }
-        len=max(len,cnt+1);
-        return len;
+        }  
+        return ans; 
     }
 };
