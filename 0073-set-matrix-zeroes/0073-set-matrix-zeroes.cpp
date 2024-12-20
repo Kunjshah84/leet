2d031b5faf5f4a15660976a988fc6b3d1761169b
Kunjshah84 <155896950+Kunjshah84@users.noreply.github.com>
2024-12-20 19:19:29 +0530

    Time: 0 ms (100%), Space: 18.4 MB (35.72%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& m) {
-        unordered_map<int,bool> row;
-        unordered_map<int,bool> col;
-        for(int i=0;i<m.size();i++){
-            for(int j=0;j<m[i].size();j++)  
-                if(!m[i][j]){
-                    row[i]=1;
-                    col[j]=1;
-                }
-        }
+        int col=1;
         for(int i=0;i<m.size();i++){
             for(int j=0;j<m[i].size();j++){
-                if(row[i] || col[j])    m[i][j]=0;
+                if(!m[i][j]){
+                    if(!j)
+                        col=0;
+                    else m[0][j]=0;
+                    m[i][0]=0;
+                }
             }
         }
+        //pahela andar nu karo bhai
+        for(int i=1;i<m.size();i++){
+            for(int j=1;j<m[i].size();j++){
+                if(m[0][j]==0 || m[i][0]==0)    m[i][j]=0;
+            }
+        }
+        if(m[0][0]==0)
+            for(int i=1;i<m[0].size();i++)  m[0][i]=0;
+        if(col==0)
+            for(int i=0;i<m.size();i++) m[i][0]=0;
     }
 };
