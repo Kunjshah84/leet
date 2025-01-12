6220dd3843bd78c7657bd762923ec8f64b0776b3
Kunjshah84 <155896950+Kunjshah84@users.noreply.github.com>
2025-01-12 22:01:29 +0530

    Time: 0 ms (100%), Space: 9.8 MB (69.29%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -2,29 +2,27 @@ class Solution {
 
     string get(int &num,int cnt){
         if(cnt==4){
-            int copy=num;
-            num-=(num-(num%1000));
-            return string(((copy-(copy%1000))/1000),'M');
+            int count=num/1000;
+            num-=(count*1000);
+            return string(count,'M');
         }
         else if(cnt==3 && num>=500){  
-            int copy=num;
             num-=500;
             return "D";
         }
         else if(cnt==3 && num<500){
-            int copy=num;
-            num-=(num-(num%100));
-            return string(((copy-(copy%100))/100),'C');
+            int count=num/100;
+            num-=(count*100);
+            return string(count,'C');
         }
         else if(cnt==2 && num>=50){
-            int copy=num;
             num-=50;
             return "L";
         }
         else if(cnt==2 && num<50){
-            int copy=num;
-            num-=(num-(num%10));
-            return string(((copy-(copy%10))/10),'X');
+            int count=num/10;
+            num-=count*10;
+            return string(count,'X');
         }   
         else if(cnt==1 && num>=5){
             num-=5;
@@ -40,10 +38,10 @@ class Solution {
             return (number==4) ? "IV" : "IX";
         } 
         else if(cnt==2){
-            num= (number==4) ? (num-40) : (num-90);
+            num=(number==4) ? (num-40) : (num-90);
             return (number==4) ? "XL" : "XC" ;
         } 
-        num= ( number==4) ? (num-400): (num-900);
+        num=(number==4) ? (num-400): (num-900);
         return (number==4) ? "CD" : "CM" ;
     }
 
@@ -57,9 +55,7 @@ public:
                 number=copy;
                 copy/=10;
             }
-            if(number==4 || number==9){
-                ans+=get_fo(num,cnt,number);
-            }
+            if(number==4 || number==9)  ans+=get_fo(num,cnt,number);
             else    ans+=get(num,cnt);
         }
         return ans;
