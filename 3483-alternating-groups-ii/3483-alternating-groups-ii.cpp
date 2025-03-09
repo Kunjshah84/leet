a4236d2c8f2646ccf2ecab006cbda868e088f73b
Kunjshah84 <155896950+Kunjshah84@users.noreply.github.com>
2025-03-09 18:15:43 +0530

    Time: 77 ms (33.47%), Space: 102.7 MB (76.78%) - LeetHub

--- 3483-alternating-groups-ii/3483-alternating-groups-ii.cpp
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int numberOfAlternatingGroups(vector<int>& nums, int k) {
-        int l=0,r=0;
-        int cnt=0;
-        while(r<(nums.size())-1){
+        int l=0,r=0,cnt=0;
+        while(r<(nums.size()-1)){
             r++;
             if(nums[r]==nums[r-1])  l=r;
             if((r-l+1) == k){
@@ -11,18 +10,11 @@ public:
                 l++;
             }
         }
-        //Now for the circular
-        cout<<cnt<<endl;
         r=-1;
         while(l<nums.size()){
             r++;
-            if(!r){
-                if(nums[r]==nums[nums.size()-1])    break ;
-                
-            }
-            else{
-                if(nums[r]==nums[r-1])  break ;
-            }
+            if(((!r) && (nums[r]==nums[nums.size()-1])) || 
+            ((r) && (nums[r]==nums[r-1])))  break ;
             if((nums.size()-l+r+1)==k){
                 cnt++;
                 l++;
