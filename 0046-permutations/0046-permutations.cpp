92025662e8c774953436c5be2eae2445dc0048c3
Kunjshah84 <155896950+Kunjshah84@users.noreply.github.com>
2025-05-02 19:22:46 +0530

    Time: 0 ms (100%), Space: 10.5 MB (87.75%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,30 +1,21 @@
 class Solution {
 
-    void get(vector<vector<int>> &ans,vector<int> &temp,
-    vector<int> &fr,vector<int> &nums){
-        if(temp.size()==nums.size()){
-            ans.push_back(temp);
+    void get(vector<vector<int>> &ans,vector<int> &nums,int ind){
+        if(ind==nums.size()){ 
+            ans.push_back(nums);
             return ;
-        }
-        for(int i=0;i<nums.size();i++){
-            if(!fr[i]){
-                fr[i]=1;
-                temp.push_back(nums[i]);
-                get(ans,temp,fr,nums);
-                fr[i]=0;
-                temp.pop_back();
-            }
+        } 
+        for(int i=ind;i<nums.size();i++){
+            swap(nums[ind],nums[i]);
+            get(ans,nums,ind+1);
+            swap(nums[ind],nums[i]);
         }
     }
-    
+
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        // This is the basic approach by using a map
-        // TC:O(n!*n)
         vector<vector<int>> ans;
-        vector<int> temp;
-        vector<int> fr(nums.size(),0);
-        get(ans,temp,fr,nums);
+        get(ans,nums,0);
         return ans;
     }
 };
