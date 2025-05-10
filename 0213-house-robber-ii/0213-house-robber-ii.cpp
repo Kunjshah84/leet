3ecdef65eb7b310c9bd687e090ff37c39b18e6f2
Kunjshah84 <155896950+Kunjshah84@users.noreply.github.com>
2025-05-10 15:18:07 +0530

    Time: 0 ms (100%), Space: 10.9 MB (40.17%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,22 +1,20 @@
 class Solution {
 
-    int get(vector<int> &nums,int ind,bool flg,vector<int> &dp1,vector<int> &dp2){
-        if(flg && ind==0)   return 0;
-        if(ind<0)   return 0;
-        if(flg && dp1[ind]!=-1) return dp1[ind];
-        if(!flg && dp2[ind]!=-1)    return dp2[ind];
-        if(ind==nums.size()-1)  flg=1;
-        int sum1=nums[ind]+get(nums,ind-2,flg,dp1,dp2);
-        if(ind==nums.size()-1)  flg=0;
-        int sum2=get(nums,ind-1,flg,dp1,dp2);
-        if(flg) return dp1[ind]=max(sum1,sum2);
-        return dp2[ind]=max(sum1,sum2);
+    int get(vector<int> &nums,int ind,int n,vector<int> &dp){
+        if(ind<n)   return 0;
+        if(dp[ind]!=-1) return dp[ind];
+        return dp[ind]=max((nums[ind]+get(nums,ind-2,n,dp)),get(nums,ind-1,n,dp));
     }
 
 public:
     int rob(vector<int>& nums) {
-        vector<int> dp1(nums.size(),-1);
-        vector<int> dp2(nums.size(),-1);
-        return get(nums,nums.size()-1,0,dp1,dp2);
+        // Just need to do the task for the two times:  
+        // Memoization tech
+        if(nums.size()==1)  return nums[0];
+        vector<int> dp(nums.size(),-1);
+        int temp1=get(nums,nums.size()-1,1,dp);
+        dp.assign(nums.size(),-1);
+        int temp2=get(nums,nums.size()-2,0,dp);
+        return max(temp1,temp2);
     }
 };
