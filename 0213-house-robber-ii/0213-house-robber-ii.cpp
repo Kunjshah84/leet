4fc72f98644ce845b38b1e7e9af0742df4018431
Kunjshah84 <155896950+Kunjshah84@users.noreply.github.com>
2025-05-10 15:36:33 +0530

    Time: 0 ms (100%), Space: 10.9 MB (21.01%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,20 +1,18 @@
 class Solution {
 
-    int get(vector<int> &nums,int ind,int n,vector<int> &dp){
-        if(ind<n)   return 0;
-        if(dp[ind]!=-1) return dp[ind];
-        return dp[ind]=max((nums[ind]+get(nums,ind-2,n,dp)),get(nums,ind-1,n,dp));
+    int get(vector<int> &nums,int n,int s){
+        vector<int> dp(n,-1);
+        dp[s]=nums[s];
+        dp[s+1]=max(nums[s],nums[s+1]);
+        for(int i=s+2;i<n;i++)  dp[i]=max((nums[i]+dp[i-2]),dp[i-1]);
+        return dp[n-1];
     }
 
 public:
     int rob(vector<int>& nums) {
-        // Just need to do the task for the two times:  
-        // Memoization tech
+        // Tebulation:
         if(nums.size()==1)  return nums[0];
-        vector<int> dp(nums.size(),-1);
-        int temp1=get(nums,nums.size()-1,1,dp);
-        dp.assign(nums.size(),-1);
-        int temp2=get(nums,nums.size()-2,0,dp);
-        return max(temp1,temp2);
+        if(nums.size()==2)  return max(nums[0],nums[1]);
+        return max(get(nums,nums.size(),1),get(nums,nums.size()-1,0));
     }
 };
