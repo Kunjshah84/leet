c73a904c99abc78ebaf1b285d6c6aabd87b0176c
Kunjshah84 <155896950+Kunjshah84@users.noreply.github.com>
2025-05-11 10:33:19 +0530

    Time: 0 ms (100%), Space: 9.3 MB (56.21%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,22 +1,21 @@
 class Solution {
-
-    // int get(int i,int j){
-    //     if(i<0 || j<0)    return 0;
-    //     if(i==0 && j==0)    return 1;
-    //     int left=get(i,j-1);
-    //     int up=get(i-1,j);
-    //     return left+up;
-    // }
-
-    int get(int i,int j,vector<vector<int>> &dp){
-        if(i<0 || j<0)  return 0;
-        if(!i && !j)    return 1;
-        if(dp[i][j]!=-1)    return dp[i][j];
-        return dp[i][j]=get(i,j-1,dp)+get(i-1,j,dp);
-    }
 public:
     int uniquePaths(int i, int j) {
         vector<vector<int>> dp(i,vector<int> (j,-1));
-        return get(i-1,j-1,dp);
+        dp[0][0]=1;
+        for(int m=0;m<i;m++){
+            for(int n=0;n<j;n++){
+                if(m || n){
+                    int left;
+                    int up;
+                    if(!m)  up=0;
+                    else up=dp[m-1][n];
+                    if(!n)  left=0;
+                    else left=dp[m][n-1];
+                    dp[m][n]=up+left;  
+                }
+            }
+        }
+        return dp[i-1][j-1];
     }
 };
