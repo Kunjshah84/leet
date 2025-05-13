db8594ba9de00b38b550900162d8370957a8bc94
Kunjshah84 <155896950+Kunjshah84@users.noreply.github.com>
2025-05-14 01:06:59 +0530

    Time: 0 ms (100%), Space: 14.9 MB (60.09%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,21 +1,20 @@
 class Solution {
-
-    int get(vector<vector<int>>& matrix,int curr,int m,vector<vector<int>> &dp){
-        if(!m)  return matrix[0][curr];
-        if(dp[m][curr]!=-100001) return dp[m][curr];
-        int s1=get(matrix,curr,m-1,dp),s2=INT_MAX,s3=INT_MAX;
-        if((curr+1)<matrix[0].size())   s2=get(matrix,curr+1,m-1,dp);
-        if((curr-1)>=0) s3=get(matrix,curr-1,m-1,dp);
-        return dp[m][curr]=matrix[m][curr]+min(s1,min(s2,s3));
-    }
-
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         // Reason behind the putting the 100001 is crazy: 
         int var=INT_MAX;
         int m=matrix.size(),n=matrix[0].size();
         vector<vector<int>> dp(m,vector<int> (n,-100001)); 
-        for(int i=0;i<n;i++)    var=min(var,get(matrix,i,m-1,dp));
+        for(int i=0;i<n;i++)    dp[m-1][i]=matrix[m-1][i];
+        for(int i=m-2;i>=0;i--){
+            for(int j=0;j<n;j++){
+                int s1=dp[i+1][j],s2=INT_MAX,s3=INT_MAX;
+                if((j+1)<n) s2=dp[i+1][j+1];
+                if((j-1)>=0)    s3=dp[i+1][j-1]; 
+                dp[i][j]=matrix[i][j]+min(s1,min(s2,s3));
+            }
+        }
+        for(int i=0;i<n;i++)    var=min(var,dp[0][i]);
         return var;
     }
 };
