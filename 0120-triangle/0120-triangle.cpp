c4d70cecfd9f38e69022bcc1624a9906bb0ccd0d
Kunjshah84 <155896950+Kunjshah84@users.noreply.github.com>
2025-05-13 21:27:37 +0530

    Time: 0 ms (100%), Space: 12.8 MB (70.96%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -4,17 +4,19 @@ public:
         int var=INT_MAX;
         int m=triangle.size();
         int n=triangle[triangle.size()-1].size();
-        vector<vector<int>> dp(m ,vector<int> (n,-1));
-        dp[0][0]=triangle[0][0];
+        vector<int> dp(n,-1);
+        dp[0]=triangle[0][0];
         for(int i=1;i<m;i++){
+            vector<int> temp=dp;
             for(int j=0;j<triangle[i].size();j++){
                 int fi=INT_MAX,se=INT_MAX;
-                if(j<triangle[i-1].size())  fi=dp[i-1][j];
-                if((j-1)>=0)    se=dp[i-1][j-1];
-                dp[i][j]=triangle[i][j]+min(fi,se);
+                if(j<triangle[i-1].size())  fi=dp[j];
+                if((j-1)>=0)    se=dp[j-1];
+                temp[j]=triangle[i][j]+min(fi,se);
             }
+            dp=temp;
         }
-        for(auto it:dp[dp.size()-1])    var=min(var,it);
+        for(auto it:dp)    var=min(var,it);
         return var;
     }
 };
