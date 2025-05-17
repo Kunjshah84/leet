104f0707fcb4add4ff3ab9909b45c6bba5bc7cd3
Kunjshah84 <155896950+Kunjshah84@users.noreply.github.com>
2025-05-17 13:48:22 +0530

    Time: 183 ms (94.61%), Space: 10.4 MB (85.82%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,29 +1,24 @@
 class Solution {
 
-    bool get(vector<vector<char>>& board, string &word,int r,int col,
-    string temp,int ind,map<pair<int,int>,int> &m){
-        if(ind>=word.size() || r<0 || col<0 || 
-        r>=board.size() || col>=board[0].size() 
-        || board[r][col]!=word[ind] || m[{r,col}])    return 0;
-        temp+=(board[r][col]);
-        if(temp.size()>word.size()) return 0;
-        if(temp==word)  return 1;
-        m[{r,col}]=1;
-        if(get(board,word,r,col-1,temp,ind+1,m) 
-        || get(board,word,r,col+1,temp,ind+1,m) 
-        || get(board,word,r+1,col,temp,ind+1,m)
-        || get(board,word,r-1,col,temp,ind+1,m))    return 1;
-        m[{r,col}]=0;
-        return 0;
-    }
+        bool get(vector<vector<char>>& board, string &word,int i,int j,int ind){
+            if(ind==word.size())    return 1;
+            if(i<0 || j<0 || i>=board.size() || j>=board[0].size() 
+            || word[ind]!=board[i][j])  return 0;
+            int temp=board[i][j];
+            board[i][j]='#';//Picked the index;
+            bool v1=get(board,word,i+1,j,ind+1);
+            bool v2=get(board,word,i-1,j,ind+1);
+            bool v3=get(board,word,i,j+1,ind+1);
+            bool v4=get(board,word,i,j-1,ind+1);
+            board[i][j]=temp;
+            return v1 || v2 || v3 || v4;
+        }
 
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        if(word.size()==0)  return 0;
-        map<pair<int,int>,int> m;
         for(int i=0;i<board.size();i++)
-            for(int j=0;j<board[i].size();j++)
-                if(board[i][j]==word[0] && get(board,word,i,j,"",0,m)) return 1;
+            for(int j=0;j<board[i].size();j++)  
+                if(board[i][j]==word[0] && get(board,word,i,j,0))  return 1;
         return 0;
     }
 };
