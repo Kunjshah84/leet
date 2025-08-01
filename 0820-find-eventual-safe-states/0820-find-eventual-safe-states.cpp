78bb670bbbd80cbdfad942cb92265ff39312c046
Kunjshah84 <155896950+Kunjshah84@users.noreply.github.com>
2025-08-01 13:55:54 +0530

    Time: 7 ms (83.3%), Space: 53.1 MB (64.48%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,32 +1,31 @@
 class Solution {
 
-int dfs(int ver,vector<vector<int>> &adj,vector<bool> &visited,vector<int> &support,
-    vector<int> &ans){
+    bool dfs(vector<int> &visited,vector<vector<int>> &graph,vector<int> &path,
+    vector<int> &cycle,int ver){
         visited[ver]=1;
-        if(!adj[ver].size()){
-            ans.push_back(ver);
-            return 1;
+        path[ver]=1;
+        for(auto it:graph[ver]){
+            if(!visited[it] && dfs(visited,graph,path,cycle,it))    return 1;
+            else if(visited[it] && path[it])   return 1;
         }
-        int cnt=0;
-        for(auto it:adj[ver]){
-            if((!visited[it] && dfs(it,adj,visited,support,ans)) 
-            || !support[it]) cnt++;         
-        }
-        support[ver]-=cnt;
-        if(!support[ver])   ans.push_back(ver);
-        return (!support[ver]) ? 1 : 0;
+        path[ver]=0;
+        cycle[ver]=1;
+        return 0;
     }
 
 public:
-    vector<int> eventualSafeNodes(vector<vector<int>>& adj) {
-        int V=adj.size();
-        vector<bool> visited(V,0);
-        vector<int> support(V);
-        for(int i=0;i<V;i++)    support[i]=adj[i].size();
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        // The another solution is that we have to find 
+        // Weather the node is a part of the cycle or the node is 
+        // contrebutiong the cycle if yes the can not going to call as the safe
+        // Node
+        vector<int> visited(graph.size(),0);
+        vector<int> path(graph.size(),0);
+        vector<int> cycle(graph.size(),0);
+        for(int i=0;i<graph.size();i++)
+            if(!visited[i]) dfs(visited,graph,path,cycle,i);  
         vector<int> ans;
-        for(int i=0;i<V;i++)
-            if(!visited[i]) dfs(i,adj,visited,support,ans);
-        sort(ans.begin(),ans.end());
+        for(int i=0;i<graph.size();i++) if(cycle[i])    ans.push_back(i);
         return ans;
     }
 };
