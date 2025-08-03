7d1332b814366d393c38887017dc88182a9e6969
Kunjshah84 <155896950+Kunjshah84@users.noreply.github.com>
2025-08-03 13:36:22 +0530

    Time: 325 ms (31.2%), Space: 21.4 MB (35.06%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        queue<pair<string,int>> q;
+        q.push({beginWord,1});
+        set<string> s;
+        for(auto it:wordList)   s.insert(it);
+        while(!q.empty()){
+            pair<string,int> temp=q.front();
+            string main_str=temp.first;
+            int old=temp.second;
+            q.pop();
+            for(int i=0;i<main_str.size();i++){
+                string copy=main_str;
+                for(int j=0;j<26;j++){
+                    copy[i]=j+'a';
+                    if(s.find(copy)!=s.end()){
+                        if(copy==endWord)   return old+1;
+                        q.push({copy,old+1});
+                        s.erase(copy);
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+};
