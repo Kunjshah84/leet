f711a17b44b152846cbb45ae50a8d2d8db171c3d
Kunjshah84 <155896950+Kunjshah84@users.noreply.github.com>
2025-08-17 12:25:07 +0530

    Time: 16 ms (88.79%), Space: 18 MB (65.87%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,35 +1,14 @@
 class Solution {
-
-    // int get(vector<int> &coins,int ind,int amount,vector<vector<int>> &dp){
-    //     if(!amount) return 0;
-    //     if(!ind){
-    //         if(coins[ind]>amount || amount%coins[ind])    return 1e9;
-    //         return amount/coins[ind];
-    //     }
-    //     if(dp[ind][amount]!=-1) return dp[ind][amount];
-    //     int nottake=get(coins,ind-1,amount,dp);
-    //     int take=1e9;
-    //     if(coins[ind]<=amount)  take=1+ get(coins,ind,amount-coins[ind],dp);
-    //     return dp[ind][amount]=min(nottake,take);
-    // }
-
 public:
     int coinChange(vector<int>& coins, int amount) {
         if(!amount) return 0;
         sort(coins.begin(),coins.end());
-        vector<vector<int>> dp(coins.size(),vector<int> (amount+1,0)); 
-        // Creating the base cases:
+        vector<int> dp(amount+1,1e9); 
         for(int i=0;i<=amount;i++)  
-            if(coins[0]>i || i%coins[0]) dp[0][i]=1e9;
-            else dp[0][i]=(i/coins[0]);
+            if(!(i%coins[0])) dp[i]=(i/coins[0]);
         for(int i=1;i<coins.size();i++)
-            for(int am=1;am<=amount;am++){
-                int nottake=dp[i-1][am];
-                int take=1e9;
-                if(am>=coins[i])    take= 1 + dp[i][am-coins[i]];
-                dp[i][am]=min(take,nottake);
-            }
-        // int ans=get(coins,coins.size()-1,amount,dp);
-        return dp[coins.size()-1][amount]==1e9 ? -1 : dp[coins.size()-1][amount]; 
+            for(int am=coins[i];am<=amount;am++)    
+                dp[am]=min((1+dp[am-coins[i]]),dp[am]);
+        return dp[amount]==1e9 ? -1 : dp[amount]; 
     }
 };
