876684bf62846da21ff85a124607fe3bf3f52395
Kunjshah84 <155896950+Kunjshah84@users.noreply.github.com>
2025-10-07 17:40:19 +0530

    Time: 7 ms (82.55%), Space: 53.1 MB (63.64%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,31 +1,31 @@
 class Solution {
+
+    bool dfs(vector<int> &visited,vector<vector<int>> &graph,vector<int> &path,
+    vector<int> &cycle,int ver){
+        visited[ver]=1;
+        path[ver]=1;
+        for(auto it:graph[ver]){
+            if(!visited[it] && dfs(visited,graph,path,cycle,it))    return 1;
+            else if(visited[it] && path[it])   return 1;
+        }
+        path[ver]=0;
+        cycle[ver]=1;
+        return 0;
+    }
+
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        // How to solve this using BFS!!
-        // We know every time we switch on the BFS from the DFS we have to use the 
-        // Topo sort kind of logic in which we push somthing ready on the queue:
-        // Now what is ready here?--->The edges which is having the 0 OD::
-        // after that we have to go to another edges so change the polarity!
-        vector<vector<int>> list(graph.size());
-        vector<int> m(graph.size(),0);
+        // The another solution is that we have to find 
+        // Weather the node is a part of the cycle or the node is 
+        // contrebutiong the cycle if yes the can not going to call as the safe
+        // Node
+        vector<int> visited(graph.size(),0);
+        vector<int> path(graph.size(),0);
+        vector<int> cycle(graph.size(),0);
         for(int i=0;i<graph.size();i++)
-            for(int j=0;j<graph[i].size();j++){
-                list[graph[i][j]].push_back(i);
-                m[i]++;
-            }
-        queue<int> q;
+            if(!visited[i]) dfs(visited,graph,path,cycle,i);  
         vector<int> ans;
-        for(int i=0;i<graph.size();i++) if(!m[i])   q.push(i);
-        while(!q.empty()){
-            int temp=q.front();
-            q.pop();
-            for(auto it:list[temp]){
-                m[it]--;
-                if(!m[it])  q.push(it);
-            }
-            ans.push_back(temp);
-        }
-        sort(ans.begin(),ans.end());
+        for(int i=0;i<graph.size();i++) if(cycle[i])    ans.push_back(i);
         return ans;
     }
 };
